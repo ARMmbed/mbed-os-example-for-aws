48a3be0d1efaad496a0f787747387897c1fbac8b
George Beckstein <donbeckstein@live.com>
2020-08-13 06:34:34 -0400

    Remove unnecessary comments in template

--- aws-cert-converter.py
+++ aws-cert-converter.py
@@ -18,36 +18,18 @@ namespace aws {{
 namespace credentials {{
 /*
  * PEM-encoded root CA certificate
- *
- * Must include the PEM header and footer,
- * and every line of the body needs to be quoted and end with \\n:
- * "-----BEGIN CERTIFICATE-----\\n"
- * "...base64 data...\\n"
- * "-----END CERTIFICATE-----";
  */
 const char rootCA[] = 
 {0};
 
 /*
  * PEM-encoded client certificate
- *
- * Must include the PEM header and footer,
- * and every line of the body needs to be quoted and end with \\n:
- * "-----BEGIN CERTIFICATE-----\\n"
- * "...base64 data...\\n"
- * "-----END CERTIFICATE-----";
  */
 const char clientCrt[] = 
 {1};
 
 /*
  * PEM-encoded client private key.
- *
- * Must include the PEM header and footer,
- * and every line of the body needs to be quoted and end with \\n:
- * "-----BEGIN RSA PRIVATE KEY-----\\n"
- * "...base64 data...\\n"
- * "-----END RSA PRIVATE KEY-----";
  */
 const char clientKey[] = 
 {2};
